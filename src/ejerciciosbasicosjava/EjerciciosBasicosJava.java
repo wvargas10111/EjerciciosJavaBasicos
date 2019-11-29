17467d5f563bdabdb8abade102db1bec2cd7b593
wellingtonvargasgonzalez <wellingtonvargasgonzalez@10.10.23.225>
2019-11-29 16:01:59 +0100

    Version de JOrge o algo asi

--- src/ejerciciosbasicosjava/EjerciciosBasicosJava.java
+++ src/ejerciciosbasicosjava/EjerciciosBasicosJava.java
@@ -29,6 +29,15 @@ public class EjerciciosBasicosJava {
         return 2;  //el resto de casos, que son que voy a más de 80
     }
     
+    public boolean muyVanidoso (int numero){
+        if (numero % 11 == 0|| numero % 11 ==1 ){
+            return true;
+    } 
+     return false;
+}
+
+
+
     /**
      * @param args the command line arguments
      */
@@ -41,6 +50,14 @@ public class EjerciciosBasicosJava {
         System.out.println( ejercicios.multa(60, false) );
         System.out.println( ejercicios.multa(65, false) );
         System.out.println( ejercicios.multa(65, true) );
+        
+        // testeso el ejercicio 3
+        
+        System.out.println("muyVanidoso (22)"+ ejercicios.muyVanidoso (22) );
+        System.out.println("muyVanidoso (23)"+ ejercicios.muyVanidoso (23) );
+        System.out.println("muyVanidoso (24)"+ ejercicios.muyVanidoso (24) );
     }
+    
+
     
 }
